22c7bca2a3ba8dd67394efa79c702fbcdc556330
TemaJava <101995680+TemaJava@users.noreply.github.com>
2023-02-10 01:10:25 +0300

    Add files via upload

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -41,3 +41,18 @@ CREATE TABLE IF NOT EXISTS friendship (
                                           user_2_id INTEGER NOT NULL,
                                           FOREIGN KEY (user_1_id) REFERENCES users (user_id) ON DELETE CASCADE,
                                           FOREIGN KEY (user_2_id) REFERENCES users (user_id) ON DELETE CASCADE);
+
+MERGE INTO mpa (mpa_id, name)
+    VALUES (1, 'G'),
+           (2, 'PG'),
+           (3, 'PG-13'),
+           (4, 'R'),
+           (5, 'NC-17');
+
+MERGE INTO genres (genre_id, name)
+    VALUES (1, 'Комедия'),
+           (2, 'Драма'),
+           (3, 'Мультфильм'),
+           (4, 'Триллер'),
+           (5, 'Документальный'),
+           (6, 'Боевик');
